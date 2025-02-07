967ed910aab61863f0d481bda5b6586b96dfac50
Hamza musema <hamzamusema26@gmail.com>
2025-02-07 20:34:14 +0300

    missing some thing

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: "https://localhost:5000",
+                url: "https://nodeback-final.onrender.com/api",
                 description: "",
             }
         ],
