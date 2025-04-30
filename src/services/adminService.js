3e6d368ee1bb3770c664591a9b501a8997a2e450
Hamza musema <hamzamusema26@gmail.com>
2025-04-30 10:51:52 +0300

    litle change

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,7 +1,7 @@
 const pool = require("../config/db");
 const OneSignal = require("onesignal-node");
 // At the top of your adminRoutes.js
-const UserService = require('../services/userService');
+const UserService = require('./userService');
 const https = require('https');
 
 // Add this before creating your OneSignal client
