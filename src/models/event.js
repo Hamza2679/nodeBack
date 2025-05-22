21bb1d4f5c07f0f98ddde2690d8e9f953eb85e8d
abdu128 <abdulaziznejo1281@gmail.com>
2025-05-23 01:48:06 +0300

    nsdam

--- src/models/event.js
+++ src/models/event.js
@@ -1,26 +1,32 @@
 class Event {
   constructor(
     id,
-    user_id,
+    userId,
     name,
     type,
     datetime,
     description,
-    cover_photo_url,
-    image_urls,
-    created_at,
-    updated_at
+    coverPhotoUrl,
+    imageUrls,
+    isOnline,
+    onlineLink,
+    onlineLinkVisible,  // ← this slot
+    createdAt,
+    updatedAt
   ) {
-    this.id = id;
-    this.user_id = user_id;
-    this.name = name;
-    this.type = type;
-    this.datetime = datetime;
-    this.description = description;
-    this.cover_photo_url = cover_photo_url;
-    this.image_urls = image_urls;
-    this.created_at = created_at;
-    this.updated_at = updated_at;
+    this.id                = id;
+    this.userId            = userId;
+    this.name              = name;
+    this.type              = type;
+    this.datetime          = datetime;
+    this.description       = description;
+    this.coverPhotoUrl     = coverPhotoUrl;
+    this.imageUrls         = imageUrls;
+    this.isOnline          = isOnline;
+    this.onlineLink        = onlineLink;
+    this.onlineLinkVisible = onlineLinkVisible;
+    this.createdAt         = createdAt;
+    this.updatedAt         = updatedAt;
   }
 }
 
