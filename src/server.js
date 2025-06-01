3a25955ee7e9b2e2b0737e55933abeb3edb61f94
Hamza2679 <abubekermurad2807@gmail.com>
2025-06-01 17:10:56 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const followRoutes = require("./routes/followRoutes");
 const studentRoutes = require("./routes/studentRoutes"); 
 const reportRoutes = require("./routes/reportRoutes");
 const groupPostReportRoutes = require('./routes/groupPostReportRoutes');
-
+const generateReportRoutes = require("./routes/generateReportRotes");
 
 const db = require("./config/db");
 const swaggerDocs = require("../swagger");
@@ -44,6 +44,7 @@ app.use("/api/events", eventRoutes);
 app.use("/api/followRo", followRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/students", studentRoutes);
+app.use("/api/generateReport", generateReportRoutes);
 
 swaggerDocs(app);
 
