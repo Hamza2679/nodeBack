f96dd7f54677b00150fbd446ccab48e7538f32b1
Hamza2679 <hamzamusema26@gmail.com>
2025-06-02 11:49:26 +0300

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -44,7 +44,8 @@ exports.initiateSignup = async (universityId) => {
             student.email, 
             otp,
             student.firstName,
-            student.lastName
+            student.lastName,
+            student.profilePictureUrl
         );
 
         return { 
@@ -88,18 +89,20 @@ exports.verifySignupOTP = async (universityId, otp) => {
 exports.completeSignup = async (universityId, password, firstName, lastName, email ,profile_picture_url) => {
     console.log("all params", universityId, password, firstName, lastName, email ,profile_picture_url);
     const client = await pool.connect();
+   
     try {
         // Hash password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         // Create user
-        const userResult = await client.query(
-            `INSERT INTO users 
-             (first_name, last_name, email, password, role, universityid ,profilepicture) 
-             VALUES ($1, $2, $3, $4, $5, $6, $7) 
-             RETURNING *`,
-            [firstName, lastName, email, hashedPassword, 'student', universityId , profile_picture_url]
-        );
+      const userResult = await client.query(
+    `INSERT INTO users 
+     (first_name, last_name, email, password, role, universityid ,profilepicture) 
+     VALUES ($1, $2, $3, $4, $5, $6, $7) 
+     RETURNING *`,
+    [firstName, lastName, email, hashedPassword, 'student', universityId , profile_picture_url]
+);
+ console.log(userResult);
 
         // Delete OTP record
         await client.query(
